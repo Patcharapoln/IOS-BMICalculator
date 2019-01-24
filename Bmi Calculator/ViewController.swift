d61e0e0050b44766ccc7abf3defc788b24c21799
Patcharapoln <patcharapol.n@ku.th>
2019-01-24 15:15:11 +0700

    Initial Commit

---
+++ Bmi Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bmi Calculator
+//
+//  Created by Mark on 24/1/2562 BE.
+//  Copyright © 2562 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
