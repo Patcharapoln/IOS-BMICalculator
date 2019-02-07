c5d91e4999c2c8ea5ee3c48133b91c307d050edd
Patcharapoln <patcharapol.n@ku.th>
2019-02-07 14:27:58 +0700

    Finished BMI calculator project

--- Bmi Calculator/ViewController.swift
+++ Bmi Calculator/ViewController.swift
@@ -9,11 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var imageResult: UIImageView!
+    @IBOutlet weak var bmi: UILabel!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageResult.image = UIImage(named: "icon.png")
+        result.text = "Please input your height and weight"
     }
+    
+    @IBAction func calculate(_ sender: Any) {
+        let height = (heightField.text! as NSString).doubleValue/100
+        let weight = (weightField.text! as NSString).doubleValue
+        let bmi = weight/pow(height,2)
+        
+        result.font = UIFont.boldSystemFont(ofSize: 17)
+        
+        if(bmi<18.5){
+            imageResult.image = UIImage(named: "underweight.png")
+            result.text = "You're underweight"
+            result.textColor = UIColor.blue
+        }
+        
+        else if(bmi>=18.5 && bmi<25){
+            imageResult.image = UIImage(named: "normal.png")
+            result.text = "You're healthy"
+            result.textColor = UIColor.green
+        }
+        
+        else if(bmi>=25 && bmi<30){
+            imageResult.image = UIImage(named: "fat.png")
+            result.text = "You're fat"
+            result.textColor = UIColor.orange
+        }
+        
+        else {
+            imageResult.image = UIImage(named: "Obese.png")
+            result.text = "You're obese"
+            result.textColor = UIColor.red
+        }
+    }
+    
 
 
 }
